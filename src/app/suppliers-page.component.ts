d70e8089190d2f1b9c1b10c4348f70214f0f3754
mkshimod <mkshimod@hawaii.edu>
2016-12-05 10:12:24 -1000

    Added empty links to nav

--- src/app/suppliers-page.component.ts
+++ src/app/suppliers-page.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'suppliers-page',
-  template: `<supplier-table></supplier-table>`
+  template: `
+    <h3>Page Title</h3>
+    <div>
+      <p>Some description of the page here</p>
+    </div>
+    <supplier-table></supplier-table>
+  `
 })
 export class SuppliersPageComponent implements OnInit {
   title = 'List of Suppliers';
